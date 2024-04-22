d13ad74b0f7bb77419e0b07e209098360f62deed
Haran Raj Madiq S <89182476+HaranMadiq@users.noreply.github.com>
2024-04-22 12:20:13 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const { encode } = require('querystring'); 
 
 // Encode the password
-const password = '1234';
-const encodedPassword = encodeURIComponent('1234', 'utf8'); 
+const password = '12345';
+const encodedPassword = encodeURIComponent('12345', 'utf8'); 
 
 // Create the MongoDB connection URL with the encoded password
-const dbURI = `mongodb+srv://himadrinayak:${password}@cluster0.h7n86ah.mongodb.net?retryWrites=true&w=majority`;
+const dbURI = `mongodb+srv://himadrinayak:${password}@cluster0.h7n86ah.mongodb.net`;
 
 // Connect to the MongoDB server with the provided URL and options 
 mongoose.connect(dbURI, {
